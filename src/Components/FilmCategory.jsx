0bd1a60c0e5f64e211220b92bba675a7b522030d
Marco <marcominisgallo@yahoo.it>
2025-04-04 15:19:08 +0200

    Extra 1

--- src/Components/FilmCategory.jsx
+++ src/Components/FilmCategory.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 import FilmRow from "./FilmRow";
 
 const URL = "http://www.omdbapi.com/?apikey=b7acc9bd&s=";
@@ -7,6 +7,8 @@ const URL = "http://www.omdbapi.com/?apikey=b7acc9bd&s=";
 class FilmCategory extends Component {
   state = {
     films: [],
+    isLoading: true,
+    isError: false,
   };
   getFilmList = () => {
     fetch(URL + this.props.query)
@@ -21,10 +23,15 @@ class FilmCategory extends Component {
         console.log(data);
         this.setState({
           films: data.Search,
+          isLoading: false,
         });
       })
       .catch((err) => {
         console.log("Errore", err);
+        his.setState({
+          isLoading: false,
+          isError: true,
+        });
       });
   };
 
@@ -36,6 +43,18 @@ class FilmCategory extends Component {
     return (
       <Container fluid>
         <h4 className="ps-5 text-light">{this.props.h4}</h4>
+        {this.state.isLoading && (
+          <div className="text-center my-3">
+            <Spinner variant="danger" animation="border" />
+          </div>
+        )}
+        {this.state.isError && (
+          <Alert variant="danger" className="text-center">
+            <i className="bi bi-patch-exclamation-fill me-2"></i>
+            Errore nel recupero dati
+            <i className="bi bi-patch-exclamation-fill ms-2"></i>
+          </Alert>
+        )}
         <FilmRow films={this.state.films} />
       </Container>
     );
